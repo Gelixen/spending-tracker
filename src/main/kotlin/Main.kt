71154e01537551a9e0b6eeb640dea12be253ee86
Karolis šlipaitis <slipaitis.karolis@gmail.com>
2023-02-25 20:30:43 +0200

    implement more sophisticated date handler

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import java.time.DateTimeException
 import java.time.LocalDate
 
 fun main() {
@@ -6,8 +7,8 @@ fun main() {
 
 class SpendingApp {
     fun format() {
-        val day = readln("Day").toInt()
-        val date = LocalDate.now().withDayOfMonth(day)
+
+        val date = promptValidDate()
 
         val amount = readln("Amount").toDouble()
 
@@ -19,6 +20,28 @@ class SpendingApp {
         val vendor = readln("Vendor")
 
         println("$date,$amount,$comment,$category,$vendor")
+    }
+
+    private fun promptValidDate(): LocalDate {
+        var date: LocalDate
+
+        while (true) {
+            val dayString = readln("Enter a payment day:")
+            val day = dayString.toIntOrNull()
+            if (day != null) {
+                try {
+                    date = LocalDate.now().withDayOfMonth(day)
+                    break
+                } catch (ex: DateTimeException) {
+                    val daysInMonth = LocalDate.now().lengthOfMonth()
+                    println("Invalid input. Only $daysInMonth days in current month")
+                }
+            } else {
+                println("Invalid input.")
+            }
+        }
+
+        return date
     }
 
     fun mapToCategoryEnum(category: String): String {
