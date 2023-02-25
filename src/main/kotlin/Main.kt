3ded5458c105f42fb3609167d10b6693824c909f
Karolis šlipaitis <slipaitis.karolis@gmail.com>
2023-02-25 09:38:00 +0200

    move everything from main to dedicated class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,26 +12,32 @@ enum class Category {
 }
 
 fun main() {
-    val day = readln("Day").toInt()
-    val date = LocalDate.now().withDayOfMonth(day)
-
-    val amount = readln("Amount").toDouble()
-
-    val comment = readln("Comment")
-
-    val categoryString = readln("Category")
-    val category = mapToCategoryEnum(categoryString)
-
-    val vendor = readln("Vendor")
-
-    println("$date,$amount,$comment,$category,$vendor")
+    SpendingApp().format()
 }
 
-fun mapToCategoryEnum(category: String): String {
-    return Category.valueOf(category.uppercase()).name
-}
+class SpendingApp {
+    fun format() {
+        val day = readln("Day").toInt()
+        val date = LocalDate.now().withDayOfMonth(day)
 
-fun readln(prompt: String): String {
-    println(prompt)
-    return readln()
+        val amount = readln("Amount").toDouble()
+
+        val comment = readln("Comment")
+
+        val categoryString = readln("Category")
+        val category = mapToCategoryEnum(categoryString)
+
+        val vendor = readln("Vendor")
+
+        println("$date,$amount,$comment,$category,$vendor")
+    }
+
+    fun mapToCategoryEnum(category: String): String {
+        return Category.valueOf(category.uppercase()).name
+    }
+
+    fun readln(prompt: String): String {
+        println(prompt)
+        return readln()
+    }
 }
